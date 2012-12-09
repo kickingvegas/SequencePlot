f8c2710fbb829dedf4b77c248fe922971df1aec3
Charles Choi <charles.choi@yummymelon.com>
2012-12-09 15:59:21 -0800

    * Added SyntaxError class exception

--- sequenceplot/__init__.py
+++ sequenceplot/__init__.py
@@ -7,6 +7,14 @@ sequenceplot is a module that generates UML sequence diagrams using the UMLGraph
 """
 __version__ = '0.1'
 
+class SyntaxError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
 from SequenceObject import SequenceObject
 from Placeholder import Placeholder
 from Actor import Actor
