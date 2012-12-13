fc4090ce9a15e575b51a8e9d53aba31e8f3d9b6b
Charles Choi <charles.choi@yummymelon.com>
2012-12-12 16:56:44 -0800

    Bump up version.

--- sequenceplot/__init__.py
+++ sequenceplot/__init__.py
@@ -19,7 +19,7 @@
 sequenceplot is a module that generates UML sequence diagrams using the UMLGraph package.
 
 """
-__version__ = '0.1'
+__version__ = '0.2'
 
 class SyntaxError(Exception):
     def __init__(self, value):
